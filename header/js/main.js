291cc642e99f147e3cd10e1f4b0698870fd71d06
skatevolley <adam29457756@gmail.com>
2023-07-21 16:25:15 +0800

    1.近期活動 - scroll Event 2.新增 月光水晶、潘朵拉、寵物箱、萌獸html架構內容

--- header/js/main.js
+++ header/js/main.js
@@ -9,33 +9,58 @@ window.onload = () =>{
             const Activitybool = ref(false)
             const handActivitybool = () =>{
                 Activitybool.value = !Activitybool.value
+                mapleActivity.scrollTop = 0;
             }
+
+ 
             // ActivityTag
             const ActivityTagData = ref([
                 {key:"黃金蘋果" ,class: "goldAppleTag" , url: "./img/黃金蘋果.png", act: true},
                 {key:"時尚隨機箱" ,class: "fashionTag" , url: "./img/時尚隨機箱.png", act: false},
-                {key:"皇家美髮券" ,class: "dressingTag" , url: "./img/皇家美髮券.png", act: false},
+                {key:"皇家美容院" ,class: "dressingTag" , url: "./img/皇家美容院.png", act: false},
                 {key:"寵物隨機箱" ,class: "petTag" , url: "./img/寵物隨機箱.png", act: false},
                 {key:"萌獸卡牌包" ,class: "cuteMosTag" , url: "./img/萌獸卡牌包.png", act: false},
                 {key:"月光水晶" ,class: "crystalTag" , url: "./img/月光水晶.png", act: false},
                 {key:"魔法畫框" ,class: "frameTag" , url: "./img/魔法畫框.png", act: false}
             ])
-            const ActivitySelected = ref([{key:"黃金蘋果", url: "./img/黃金蘋果_banner.png"}])
+            const ActivitySelected = ref([{key:"黃金蘋果", bannerUrl: "./img/黃金蘋果_banner.png"}])
 
             const handActivityTag = (el) =>{
                 let target = el.currentTarget.dataset.tag
                 ActivityTagData.value = ActivityTagData.value.map(item=>{
                     if(target === item.key){
                         ActivitySelected.value[0].key = item.key;
-                        ActivitySelected.value[0].url = `./img/${item.key}_banner.png`;
-                        console.log(ActivitySelected.value[0].key );
-                        console.log(ActivitySelected.value[0].url );
+                        ActivitySelected.value[0].bannerUrl = `./img/${item.key}_banner.png`;
                         return {key: item.key ,class: item.class , url: item.url , act: true}
                     }else{
                         return {key: item.key ,class: item.class , url: item.url , act: false}
                     }
                 })
             }
+
+                
+                // 近期活動 - scroll Event
+                const scrollContainer = ref(null);
+                const scrollValue = ref(0);
+                function onScroll() {
+                    scrollValue.value = scrollContainer.value.scrollTop;
+                  }        
+                  const handscrollTop = () =>{
+                      scrollContainer.value.scrollTop = 0
+                  }                
+                              
+
+                onMounted(() => {
+                  scrollValue.value = scrollContainer.value.scrollTop;
+                });
+
+                // 使用onUpdated來在組件更新後檢測滾動變化
+                onUpdated(() => {
+                  onScroll()
+                });
+
+
+    
             return{ 
                 // Activitybool
                 Activitybool,
@@ -44,6 +69,11 @@ window.onload = () =>{
                 ActivityTagData,
                 ActivitySelected,
                 handActivityTag,
+                // Activityscroll
+                scrollContainer,
+                scrollValue,
+                onScroll,
+                handscrollTop,
             }   
         }
     }
