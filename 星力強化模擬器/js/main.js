c07806dc54d67bf9f6b876d4516a7fd6a5dbf181
skatevolley <adam29457756@gmail.com>
2023-07-25 16:15:08 +0800

    基本排版架構

--- 星力強化模擬器/js/main.js
+++ 星力強化模擬器/js/main.js
@@ -5,9 +5,37 @@ window.onload = () =>{
     const App = {
 
         setup(){
-            const test = ref(5207)
+            // 楓幣台幣 input限制
+            const NTDtransMaplecoin = ref("")
+            const MaplecointransPoint = ref("")
+            const equipcost = ref("")
+            const starcost = ref("")
+            const InputNumOnly = (el)=>{
+                el.target.value = el.target.value.replace(/[^0-9]/g, "");
+            }
+
+            // 裝備星力策略選擇
+            const equipSelected = ref("請選擇")
+            const starSelected = ref("請選擇")
+            const strategySelected = ref("請選擇")
+
+            // 衝22以上的裝備數
+            const equipItem = ref(1)
+
+
             return{
-                test
+                // 楓幣台幣 input限制
+                NTDtransMaplecoin,
+                MaplecointransPoint,
+                equipcost,
+                starcost,
+                InputNumOnly,
+                // 裝備星力選擇
+                equipSelected,
+                starSelected,  
+                strategySelected,   
+                // 衝22以上的裝備數
+                equipItem         
             }   
         },
 
