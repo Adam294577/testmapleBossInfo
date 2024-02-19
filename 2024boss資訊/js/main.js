c8d1fc11b697a70cf2c372e27ea66588dba3e858
Satoru <adam29457756@gmail.com>
2024-02-19 20:32:17 +0800

    1. 增加 seo meta、 tilte圖示 2. 新增 nav改變 boss名稱、boss介紹圖片 功能

--- 2024boss資訊/js/main.js
+++ 2024boss資訊/js/main.js
@@ -5,7 +5,46 @@ window.onload = () =>{
     const App = {
 
         setup(){
-            return{}   
+            const BossNameData = reactive({is:[]})
+            const BossNavCont = ref({is:[]})
+            const BossNavContBool = ref(false)
+            const handBossNavCont  = (el= null , key) =>{
+                if(key ==='close') return BossNavContBool.value = false
+                let data = BossNameData.is.data
+                let result = data[key]
+                BossNavCont.value.is = result
+                BossNavContBool.value = true
+                console.log(BossNavCont.value.is);
+            }
+            const nowBossRender = ref({name:"守護天使綠水靈",difficulty:"普通"})
+            const handNowBossRender = (el= null , key, difficulty)=>{
+                nowBossRender.value.name = key
+                nowBossRender.value.difficulty = difficulty
+                BossNavContBool.value = false
+                console.log('現在難度:',nowBossRender.value.difficulty);
+            }
+            onMounted(()=>{
+                const api = axios.create({
+                    baseURL: './api/',
+                  });
+    
+                async function GetBossNameApiData(){
+                    try{
+                        const res = await api.get('bossData.json')
+                        BossNameData.is = res.data
+                    }catch{
+                        console.error('沒接到 bossName Api');
+                    }
+                }
+                GetBossNameApiData()
+            })
+            return{
+                handBossNavCont,
+                BossNavCont,
+                BossNavContBool,
+                nowBossRender,
+                handNowBossRender,
+            }   
         },
 
     }
