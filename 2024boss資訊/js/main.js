4a06c702240143b316e56064b3f455fcbfe8a312
Satoru <adam29457756@gmail.com>
2024-02-19 23:11:38 +0800

    新增 難度模式的切換功能

--- 2024boss資訊/js/main.js
+++ 2024boss資訊/js/main.js
@@ -14,21 +14,46 @@ window.onload = () =>{
                 let result = data[key]
                 BossNavCont.value.is = result
                 BossNavContBool.value = true
-                console.log(BossNavCont.value.is);
+                // console.log(BossNavCont.value.is);
             }
-            const nowBossRender = ref({name:"守護天使綠水靈",difficulty:"普通"})
+            const nowBossRender = ref({name:"巴洛古",difficulty:"簡單"})
             const handNowBossRender = (el= null , key, difficulty)=>{
                 nowBossRender.value.name = key
                 nowBossRender.value.difficulty = difficulty
                 BossNavContBool.value = false
-                console.log('現在難度:',nowBossRender.value.difficulty);
+                window.scrollTo({top:0})
+                // console.log('現在難度:',nowBossRender.value.difficulty);
             }
+            const BossDifficultyData = reactive({is:[]})
+
+            const handBossDifficulty = ()=>{
+                let data = BossDifficultyData.is.data
+                data = data.filter(item=>{
+                    if(item.bossName === nowBossRender.value.name) return item
+                })
+                let nowIdx = 0
+                console.log(data[0].Difficulty.length);
+                if(data[0].Difficulty.length === 1) return  console.log('只有1個 不會變難度');
+                data[0].Difficulty.forEach(item=>{
+                    if(item.is === nowBossRender.value.difficulty){
+                        nowIdx = item.idx
+                    }
+                })
+                console.log(nowIdx);
+                if(data[0].Difficulty.length -1 === nowIdx){
+                    nowIdx = 0
+                }else{
+                    nowIdx++
+                }
+                nowBossRender.value.difficulty = data[0].Difficulty[nowIdx].is
+            }
+            
             onMounted(()=>{
                 const api = axios.create({
                     baseURL: './api/',
                   });
     
-                async function GetBossNameApiData(){
+                async function GetBossNameApi(){
                     try{
                         const res = await api.get('bossData.json')
                         BossNameData.is = res.data
@@ -36,7 +61,17 @@ window.onload = () =>{
                         console.error('沒接到 bossName Api');
                     }
                 }
-                GetBossNameApiData()
+                async function GetBossDifficultyDataApi(){
+                    try{
+                        const res = await api.get('bossDifficulty.json')
+                        BossDifficultyData.is = res.data
+                        console.log(BossDifficultyData.is);
+                    }catch{
+                        console.error('沒接到 bossDifficulty Api');
+                    }
+                }
+                GetBossNameApi()
+                GetBossDifficultyDataApi()
             })
             return{
                 handBossNavCont,
@@ -44,6 +79,8 @@ window.onload = () =>{
                 BossNavContBool,
                 nowBossRender,
                 handNowBossRender,
+
+                handBossDifficulty,
             }   
         },
 
